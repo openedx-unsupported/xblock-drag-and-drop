ec8d7eb6d0eebcfcd065c80ebc5d39894ea0d95c
dragonfi <david.gabor.bodor@gmail.com>
2014-08-11 13:48:56 +0200

    Remove redundant ids from events

--- drag_and_drop/drag_and_drop.py
+++ drag_and_drop/drag_and_drop.py
@@ -200,9 +200,6 @@ class DragAndDropBlock(XBlock):
         except KeyError as e:
             return {'result': 'error', 'message': 'Missing event_type in JSON data'}
 
-        data['component_id'] = self.scope_ids.usage_id
-        data['user_id'] = self.runtime.user_id
-
         self.runtime.publish(self, event_type, data)
         return {'result':'success'}
 
@@ -316,8 +313,6 @@ class DragAndDropBlock(XBlock):
                 pass
 
         self.runtime.publish(self, 'drag-and-drop.item.dropped', {
-            'component_id': self.scope_ids.usage_id,
-            'user_id': self.runtime.user_id,
             'item_id': item_id,
             'location': bucket_id,
             'is_correct': is_correct
